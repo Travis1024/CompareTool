6a24b32b52390ebaeac83549d761b8c3b6a96fdd
Travis1024 <travis_x@163.com>
2023-07-25 20:28:52 +0800

    完成 maven 对比

---
+++ src/main/java/com/travis/hust/comparetool/pojo/dto/CompareResult.java
@@ -0,0 +1,25 @@
+package com.travis.hust.comparetool.pojo.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * @ClassName CompareResult
+ * @Description TODO
+ * @Author travis-wei
+ * @Version v1.0
+ * @Data 2023/7/25
+ */
+@Data
+public class CompareResult implements Serializable {
+    private Integer targetClassSum;
+    private Integer jarClassSum;
+    private Integer missingClassNumber;
+    private Integer differentClassNumber;
+    private Integer sameClassNumber;
+
+    private Set<String> missingClassSet;
+    private Set<String> differentClassSet;
+}
--- src/main/java/com/travis/hust/comparetool/service/impl/CompareMavenServiceImpl.java
+++ src/main/java/com/travis/hust/comparetool/service/impl/CompareMavenServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.crypto.digest.DigestUtil;
 import com.travis.hust.comparetool.enums.BizCodeEnum;
+import com.travis.hust.comparetool.pojo.dto.CompareResult;
 import com.travis.hust.comparetool.service.CompareService;
 import com.travis.hust.comparetool.utils.R;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @ClassName CompareServiceImpl
@@ -26,16 +29,47 @@ public class CompareMavenServiceImpl implements CompareService {
 
     @Override
     public R<?> compareClassFile(String rootPath, List<String> jarPathList) throws IOException {
+
+        /**
+         * 准备工作：mvn clean
+         */
+        Process execkedClean = RuntimeUtil.exec("mvn clean -f " + rootPath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(execkedClean.getInputStream()));
+
+        // 记录当前行内容
+        String line = null;
+        // 记录最后 10 行编译输出
+        Deque<String> deque = new LinkedList<>();
+
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            // 保留最后 10 行输出
+            if (deque.size() > 10) {
+                deque.removeFirst();
+            }
+            deque.add(line);
+        }
+
+        // 判断是否 clean 成功
+        boolean flag = false;
+        for (String temp : deque) {
+            if (temp.contains("BUILD SUCCESS")) {
+                log.info("[ Clean 成功！]");
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) return R.error(BizCodeEnum.UNKNOW, "工程源代码 Clean 失败！");
+
+
         /**
          * 一、发送编译命令（起 websocket 实时返回编译结果）
          */
         Process execked = RuntimeUtil.exec("mvn compile -f " + rootPath);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(execked.getInputStream()));
 
-        // 记录当前行内容
-        String line = null;
-        // 记录最后 10行编译输出
-        Deque<String> deque = new LinkedList<>();
+        // 记录最后 10 行编译输出
+        deque.clear();
 
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
@@ -47,7 +81,7 @@ public class CompareMavenServiceImpl implements CompareService {
         }
 
         // 判断是否编译成功
-        boolean flag = false;
+        flag = false;
         for (String temp : deque) {
             if (temp.contains("BUILD SUCCESS")) {
                 log.info("[编译成功！]");
@@ -86,14 +120,80 @@ public class CompareMavenServiceImpl implements CompareService {
             }
         }
 
-        // 解压 所有 jar 包，计算 jar 包下的 class 文件md5值
+
+        /**
+         * 三、解压 所有 jar 包，计算 jar 包下的 class 文件 md5 值
+         */
+        Map<String, List<String>> jarClassMap = new HashMap<>();
+        int jarClassSum = 0;
+
+        for (String oneJarPath : jarPathList) {
+            // 判断传入的 jar 包是否存在
+            if (!FileUtil.exist(oneJarPath)) continue;
+
+            JarFile jarFile = new JarFile(oneJarPath);
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry jarEntry = entries.nextElement();
+                if (!jarEntry.isDirectory() && jarEntry.getName().endsWith(".class")) {
+                    // 计数+1
+                    jarClassSum++;
+                    // 获取 class 文件名
+                    String jarEntryName = jarEntry.getName();
+                    InputStream inputStream = jarFile.getInputStream(jarEntry);
+                    // 计算文件 md5 值
+                    String md5Hex = DigestUtil.md5Hex(inputStream);
+
+                    if (jarClassMap.containsKey(jarEntryName)) {
+                        jarClassMap.get(jarEntryName).add(md5Hex);
+                    } else {
+                        List<String> arrayList = new ArrayList<>();
+                        arrayList.add(md5Hex);
+                        jarClassMap.put(jarEntryName, arrayList);
+                    }
+                }
+            }
+        }
 
 
-        // 对比 所有文件的md5值
+        /**
+         * 四、对比两个 map 所有文件的 md5 值
+         */
+        Set<String> missingClassSet = new HashSet<>();
+        Set<String> differentClassSet = new HashSet<>();
+        int sameNumber = 0;
 
+        for (String key : jarClassMap.keySet()) {
+            if (targetClassMap.containsKey(key)) {
+                List<String> jarClassMd5List = jarClassMap.get(key);
+                List<String> targetClassMd5List = targetClassMap.get(key);
+                for (String oneMd5 : jarClassMd5List) {
+                    if (targetClassMd5List.contains(oneMd5)) {
+                        sameNumber++;
+                    } else {
+                        differentClassSet.add(key);
+                        break;
+                    }
+                }
+            } else {
+                missingClassSet.add(key);
+            }
+        }
 
-        return null;
+        /**
+         * 封装对比结果
+         */
+        CompareResult compareResult = new CompareResult();
+        compareResult.setMissingClassSet(missingClassSet);
+        compareResult.setDifferentClassSet(differentClassSet);
+
+        compareResult.setJarClassSum(jarClassSum);
+        compareResult.setTargetClassSum(fileList.size());
+
+        compareResult.setSameClassNumber(sameNumber);
+        compareResult.setMissingClassNumber(missingClassSet.size());
+        compareResult.setDifferentClassNumber(jarClassSum - sameNumber - missingClassSet.size());
+
+        return R.success(compareResult);
     }
-
-
 }
